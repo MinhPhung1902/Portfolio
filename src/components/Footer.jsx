b0b469e61ae49bbd82cd9a9a5eb1fec8f1a5ff93
MinhPhung1902 <90078561+MinhPhung1902@users.noreply.github.com>
2022-02-13 20:31:42 +0700

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faFacebookF , } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
@@ -14,12 +14,22 @@ const Footer = () => {
     <h1 className='text-gray-400 text-sm'>Developed by Kanye Phung. All rights reserved.</h1>
     </div>
     <div className="flex flex-col space-y-2">
-      <div className='flex flex-row space-x-3'>
-      <FontAwesomeIcon icon={faFacebookF}  className='text-gray-400 h-6 w-6 cursor-pointer'/>
-      <FontAwesomeIcon icon={faInstagram} className='text-gray-400 h-6 w-6 cursor-pointer'/>
-      <FontAwesomeIcon icon={faLinkedin} className='text-gray-400 h-6 w-6 cursor-pointer'/>
-      <FontAwesomeIcon icon={faGithubSquare} className='text-gray-400 h-6 w-6 cursor-pointer'/>
-      <FontAwesomeIcon icon={faEnvelope} className='text-gray-400 h-6 w-6 cursor-pointer'/>
+      <div className='flex flex-row space-x-3'>       
+            <a href='https://www.facebook.com/nhatminh.phung.192/'>
+            <FontAwesomeIcon icon={faFacebookF}  className='text-gray-400 h-6 w-6 cursor-pointer hover:text-white'/>
+            </a>
+            <a href='https://www.instagram.com/kanye_phung/'>
+            <FontAwesomeIcon icon={faInstagram}  className='text-gray-400 h-6 w-6 cursor-pointer hover:text-white'/>
+            </a>
+            <a href='https://www.linkedin.com/in/minhnhat19/'>
+            <FontAwesomeIcon icon={faLinkedin}  className='text-gray-400 h-6 w-6 cursor-pointer hover:text-white'/>
+            </a>
+            <a href='https://github.com/MinhPhung1902'>
+            <FontAwesomeIcon icon={faGithub}  className='text-gray-400 h-6 w-6 cursor-pointer hover:text-white'/>
+            </a>               
+      </div>
+      <div>
+        <h1 className='text-gray-400 text-sm'>Email: phungnhatminh1902@gmail.com</h1>
       </div>
       <div className='flex flex-row space-x-2'>
         <h1 className='text-gray-400 hover:text-white cursor-pointer text-sm'>About Me</h1>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Projects = () => {
   const project = [
-    {id:1, title:'Modern UI restaurant website', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'Tailwindcss', img:'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJlc3RhdXJhbnQlMjB3ZWJzaXRlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1400&q=60'},
-    {id:2, title:'Movie App', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'axios', tech3:'MaterialUI', img:'https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW92aWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'},
-    {id:3, title:'E-commerce', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'NextJS', tech2:'Tailwindcss', tech3:'Shopify', img:'https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZSUyMGNvbW1lcmNlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60'},
-    {id:4, title:'Weather App', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'Sass', img:'https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VhdGhlcnxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'},
-    {id:5, title:'Chat app', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'antd', tech2:'firebase', img:'https://images.unsplash.com/photo-1519069060891-f8c50519bf39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hhdCUyMGFwcHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60' },
+    {id:1, title:'Modern UI restaurant website', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'Tailwindcss', img:'https://images.unsplash.com/photo-1519671282429-b44660ead0a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJlc3RhdXJhbnQlMjB3ZWJzaXRlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=1400&q=60', link:'https://thehanoians.netlify.app/'},
+    {id:2, title:'Movie App', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'axios', tech3:'MaterialUI', img:'https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW92aWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60', link:'https://movietown.netlify.app//'},
+    {id:3, title:'E-commerce', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'NextJS', tech2:'Tailwindcss', tech3:'Shopify', img:'https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZSUyMGNvbW1lcmNlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60', link:'https://houseofphungs.netlify.app//'},
+    {id:4, title:'Weather App', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'Sass', img:'https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VhdGhlcnxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60', link:'https://thehanoians.netlify.app/'},
+    {id:5, title:'Chat app', description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque tempore non  in repudiandae rem veritatis cum ducimus.', tech1:'ReactJS', tech2:'antd', tech2:'firebase', img:'https://images.unsplash.com/photo-1519069060891-f8c50519bf39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hhdCUyMGFwcHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60', link:'https://thehanoians.netlify.app/' },
   ]
   return <div className='mt-10 flex flex-col items-center justify-center space-y-5 font-sans'>
     <div className='w-10 h-10 rounded-full bg-gradient-to-r  from-pink-500 to-fuchsia-700 text-black font-sans'>
@@ -44,7 +45,7 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="flex items-center justify-between py-4">
-                        <h1 class="text-pink-500 hover:text-fuchsia-700 cursor-pointer text-xs font-semibold">Visit demo</h1>
+                        <a href={e.link} class="text-pink-500 hover:text-fuchsia-700 cursor-pointer text-xs font-semibold">Visit demo</a>
                     </div>
                 </div>
             </div>
@@ -52,6 +53,13 @@ const Projects = () => {
           )
         })}
       </div>
+        {/* <div className='flex items-center justify-center'>
+        <button className="px-[23px] py-1 bg-white text-black text-[18px] rounded-[20px] cursor-pointer hover:bg-black hover:border-gray-400 border-[1px] hover:text-gray-400">
+          <Link to='/Projects'>
+          See more
+          </Link>
+        </button>
+        </div> */}
   </div>;
 };
 
